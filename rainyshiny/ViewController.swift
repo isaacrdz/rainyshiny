a8d72dadb6e624b7fa6a0afe28106d78e2ac2721
isaacrdz <zack_cpp@hotmail.com>
2017-09-12 17:08:00 -0500

    Initial Commit

---
+++ rainyshiny/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rainyshiny
+//
+//  Created by Isaac Rodriguez on 9/12/17.
+//  Copyright © 2017 Isaac Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
